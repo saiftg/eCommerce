47935cdd6f4063ccf7aaa50587214ef07df83f9f
Saif M <smahmud46@yahoo.com>
2017-12-11 15:23:28 -0500

    build slider

--- front-end/src/components/SlickSlider.js
+++ front-end/src/components/SlickSlider.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 class SlickSlider extends Component{
 	render(){
+		const settings = {
+			dots: true,
+			infinite: true,
+			speed: 500,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true
+		}
 		return(
-			<h1>Slider ova hurr</h1>
-			)
+		<div>
+		<Slider {...settings}>
+		<div className="slick-image"><img src="/slider-images/ferrari.jpg"/></div>
+		<div className="slick-image"><img src="/slider-images/lamb.jpg"/></div>
+		<div className="slick-image"><img src="/slider-images/train1.jpg"/></div>
+		<div className="slick-image"><img src="/slider-images/schooner.jpg"/></div>
+		</Slider>
+		</div>
+		)
 	}
 }
 
