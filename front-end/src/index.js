0f0786f6b86cd58b399929ec0490c1f072828197
Saif M <smahmud46@yahoo.com>
2017-12-12 10:29:34 -0500

    add react-redux mod, provider comp, finish store and complete authreducer

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -7,11 +7,22 @@ import { createStore, applyMiddleware } from 'redux';
 import RootReducer from './reducers/RootReducer';
 import reduxPromise from 'redux-promise';
 
-// import Student from './Student';
-// import registerServiceWorker from './registerServiceWorker';
+//We need to rell React abt redux
+import { Provider } from 'react-redux';
+
+
+//create the store
+const theStore = applyMiddleware(reduxPromise)(createStore)(RootReducer);
+
+
+//Hand render the provider and hand provider theStore.
+//Put app inside of theProvider, so that everything inside of app, will know abt
+//Provider/theStore
+
 
 ReactDOM.render(
-	<App />, 
+	<Provider store={theStore}>
+	<App /> 
+	</Provider>,
 	document.getElementById('root'
 ));
-// registerServiceWorker();
---
+++ front-end/src/reducers/AuthReducer.js
@@ -0,0 +1,7 @@
+//A reducer is a function that returns a piece of state
+
+
+
+export default function(state=[], action){
+	return state
+}
